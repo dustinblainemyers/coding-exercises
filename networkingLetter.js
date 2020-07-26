f44624ab731e3ce8ac66474ba2c085442eaacb37
Dustin Myers <dustinblaine.myers@gmail.com>
2020-07-26 07:46:34 -0400

    networking letter

---
+++ networkingLetter.js
@@ -0,0 +1,7 @@
+function createDigitalCraftsLetter(firstName, company, customStringAboutThem) {
+  let letterBody = `Hey  ${firstName}, \n\t
+    I have been learning web development for a while now and I recently graduated from DigitalCrafts coding bootcamp.  You seem to be where I want to be`;
+  console.log(letterBody);
+}
+
+createDigitalCraftsLetter("Test");
